5873c80607ead1f8897fcf5f1e6f0c7c9f661ed9
Giacomo Costantini <jack@192.168.1.185>
2026-05-20 18:34:39 +0200

    fix card padding, menu mobile, aggiunta newsletter

--- dist/_astro/hoisted.Bi_jJWJg.js
+++
@@ -1,1 +0,0 @@
-const e=document.getElementById("mobile-menu-toggle"),t=document.getElementById("mobile-menu");e?.addEventListener("click",()=>{t?.classList.toggle("hidden")});
---
+++ dist/_astro/hoisted.DIT6FMOu.js
@@ -0,0 +1,1 @@
+import"./Header.astro_astro_type_script_index_0_lang.CpF05etI.js";import"https://identity.netlify.com/v1/netlify-identity-widget.js";
