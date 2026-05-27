0a674f7278b2ff6c55faceca5a18cbe4f3f82433
Giacomo Costantini <jack@192.168.1.185>
2026-05-27 15:24:51 +0200

    aggiunta generazione sitemap automatica

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  site: 'https://tramastudio.media',
+  integrations: [tailwind(), mdx(), sitemap()],
   output: 'static',
 });
